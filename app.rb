909407d21668ecdd1dbdd658fbe0dc93ed3c7753
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-08 01:34:30 -0500

    orlando

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'http'
-require 'guard'
-require 'rack-livereload'
+#require 'guard'
+#require 'rack-livereload'
 
+@@key = "AIzaSyC8xY7H9mf9HVrAmFPcb66v9hWeW4asmBU"
+helpers do  
+    def get_books(params)
+        JSON.parse(HTTP.get("https://www.googleapis.com/books/v1/volumes?q=#{params}&key=#{@@key}").body)
+    end
+end
 
 get "/" do
 erb :index
 end
 
 get "/search" do
-erb :search
+    @search = params.fetch("q",'').split(" ").join("+")
+    puts "Hola #{@search}"
+    @books = @search.empty? ? [] : get_books(@search)
+    erb :search
 end
 
 get "/books/:book_id" do
