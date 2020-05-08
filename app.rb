7eb73888a340177a8709b97a31fc69eea06602ab
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-05-08 15:52:59 -0500

    add books

--- app.rb
+++ app.rb
@@ -1,17 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'http'
-require 'guard'
-require 'rack-livereload'
+#require 'guard'
+#require 'rack-livereload'
+require 'Lazyrecord'
+require 'date'
+require_relative 'model/Book'
+#use Rack::LiveReload if development?
 
 @@key = "AIzaSyC8xY7H9mf9HVrAmFPcb66v9hWeW4asmBU"
 helpers do  
     def get_books(params)
         JSON.parse(HTTP.get("https://www.googleapis.com/books/v1/volumes?q=#{params}&key=#{@@key}").body)
     end
-end
 
-use Rack::LiveReload if development?
+    def get_specific_book(id)
+        JSON.parse(HTTP.get("https://www.googleapis.com/books/v1/volumes/#{id}?key=#{@@key}").body)
+    end
+
+    def get_params(id)
+        book_json = get_specific_book(id)
+        arr = {}
+        arr["img"] = book_json["volumeInfo"]["imageLinks"]["thumbnail"]
+        arr["title"] = book_json["volumeInfo"]["title"]
+        arr["author"] = book_json["volumeInfo"]["authors"]
+        arr
+    end
+end
 
 get "/" do
 erb :index
@@ -19,9 +34,16 @@ end
 
 get "/search" do
     @search = params.fetch("q",'').split(" ").join("+")
-    puts "Hola #{@search}"
     @books = @search.empty? ? [] : get_books(@search)
     erb :search
+end
+
+get "/search/:id&:status" do
+    id = params["id"]
+    status = params["status"]
+    par = get_params(id)
+    Book.create(par["img"], par["title"], par["author"], status)
+    redirect "books"
 end
 
 get "/books/:book_id" do
@@ -30,7 +52,8 @@ get "/books/:book_id" do
 end
 
 get "/books" do
-erb :books
+    @my_books = Book.all
+    erb :books
 end
 
 get "/books/:book_id/edit" do
